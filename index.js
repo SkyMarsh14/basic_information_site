3b62701e2b3010f842da1860560be2a478f9719e
Sky Randy <soras1758@gmail.com>
2025-02-20 05:03:57 +0900

    refactored with express for the better readability

--- index.js
+++ index.js
@@ -1,26 +1,20 @@
-const http = require("http");
-const url = require("url");
+const express = require("express");
 const fs = require("fs");
-http
-  .createServer(function (req, res) {
-    const pageUrl = url.parse(req.url, true);
-    let filename = "." + pageUrl.pathname + ".html";
+const app = express();
 
-    if (pageUrl.href === "/") {
-      filename = "./index.html";
-    }
-    fs.readFile(filename, function (err, data) {
-      if (err) {
-        res.writeHead(404, { "Content-Type": "text/html" });
-        fs.readFile("./404.html", (err, data) => {
-          res.write(data);
-          res.end();
-        });
-        return;
-      }
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
-      return res.end();
-    });
-  })
-  .listen(8080);
+app.get("/", (req, res) => {
+  res.sendFile("./index.html", {
+    root: __dirname,
+  });
+});
+app.get("/:id", (req, res) => {
+  res.sendFile(`./${req.params.id}.html`, { root: __dirname }, (err) => {
+    res.status(404).sendFile("./404.html", { root: __dirname });
+    console.log("The specified route doesn't exist.");
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`My first express app - Listening on Port ${PORT}`);
+});
